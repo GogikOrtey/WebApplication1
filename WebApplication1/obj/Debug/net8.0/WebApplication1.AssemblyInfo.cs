3dd7fd025ee327d656c7d67e039ac4109ab08f1c
Georg Orlov <orlov@altek.online>
2025-01-16 10:42:02 +0500

    Add more correct descr

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86d9bc5a4ec3f449fac76cf7debd2976039306d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b87300622b2bac2cc17db164f1f1ef9f46ed4fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
